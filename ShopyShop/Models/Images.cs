4e872195e81041ad6e3a3e96141020ff97498ca8
QLeech <c-yleech@rambler.ru>
2016-01-21 03:08:00 +0500

    add part of image table model

---
+++ ShopyShop/Models/Images.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopyShop.Models
+{
+    public class Images
+    {
+        public uint Id { get; set; }
+        
+    }
+}
